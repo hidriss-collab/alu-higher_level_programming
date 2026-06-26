fb3dd1ba0ff82f03a09625fc8cf9666ad83645e3
hidriss-collab <h.idriss@alustudent.com>
2026-06-26 09:08:57 -0700

    Fix 6-concat.py location and content

--- 6-concat.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-str1 = "Welcome to Holberton"
-str2 = "School!"
-message = f"{str1} {str2}"
-print(message)
--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
+str1 = "Welcome to Holberton"
 str2 = "School!"
-message = f"{str1} Holberton {str2}"
+message = f"{str1} {str2}"
 print(message)
