5e5ef68b6980c6b30abba5d0f163d9d333599093
hidriss-collab <h.idriss@alustudent.com>
2026-07-28 01:53:35 -0700

    Complete inheritance tasks 0-3

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines MyList."""
+
+
+class MyList(list):
+    """A class that inherits from list."""
+
+    def print_sorted(self):
+        """Print the list sorted in ascending order."""
+        print(sorted(self))
