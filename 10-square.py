6dab29385d3527a340645fe85e123f587f050573
hidriss-collab <h.idriss@alustudent.com>
2026-07-28 02:03:39 -0700

    Complete inheritance tasks 8-11

---
+++ 10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that defines Square."""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square class."""
+
+    def __init__(self, size):
+        """Initialize a square."""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
